0481339ba85d7fbac5bee32fb7d5eb9877f25a91
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-21 16:52:23 +0200

    add manager app to settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "manager",
 ]
 
 ROOT_URLCONF = "task_manager.urls"
