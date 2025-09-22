1e1bcf6bb70033e6fab427584f6de961d053e526
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-22 20:44:01 +0200

    update manager/views.py-add views for TaskTypeList/Create/Update/Delete

--- manager/views.py
+++ manager/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from manager.models import Task, Position
+from manager.models import Task, Position, TaskType
 
 User = get_user_model()
 
@@ -60,4 +60,50 @@ class PositionDeleteView(generic.DeleteView):
         )
         return context
 
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    template_name = "manager/task_type_list.html"
+    context_object_name = "task_type_list"
+
+
+class TaskTypeCreateView(generic.CreateView):
+    model = TaskType
+    template_name = "manager/task_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-type-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["previous_url"] = self.request.META.get(
+            "HTTP_REFERER", reverse_lazy("manager:task-type-list")
+        )
+        return context
+
+
+class TaskTypeUpdateView(generic.UpdateView):
+    model = TaskType
+    template_name = "manager/task_type_form.html"
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-type-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["previous_url"] = self.request.META.get(
+            "HTTP_REFERER", reverse_lazy("manager:task-type-list")
+        )
+        return context
+
+
+class TaskTypeDeleteView(generic.DeleteView):
+    model = TaskType
+    template_name = "manager/task_type_confirm_delete.html"
+    success_url = reverse_lazy("manager:task-type-list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["previous_url"] = self.request.META.get(
+            "HTTP_REFERER", reverse_lazy("manager:task-type-list")
+        )
+        return context
 
