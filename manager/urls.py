11210f6aa101c3a4553724455524679de847db20
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-22 19:42:44 +0200

    update urls.py-add paths to PositionCreate/Update/DeleteView

--- manager/urls.py
+++ manager/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from manager.views import index, PositionListView
+from manager.views import (
+    index,
+    PositionListView,
+    PositionCreateView,
+    PositionUpdateView,
+    PositionDeleteView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("positions/", PositionListView.as_view(), name="position-list"),
+    path("positions/create/", PositionCreateView.as_view(), name="position-create"),
+    path("positions/<int:pk>/update/", PositionUpdateView.as_view(), name="position-update"),
+    path("positions/<int:pk>/delete/", PositionDeleteView.as_view(), name="position-delete"),
 ]
 
 
