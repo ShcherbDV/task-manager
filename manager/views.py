2b56d05f5056a008645f848fa790ba5a0839ec8d
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-22 17:19:10 +0200

    update views.py, add index function

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Task
+
+
+User = get_user_model()
+
+def index(request):
+    num_of_tasks = Task.objects.count()
+    num_of_workers = User.objects.count()
+
+    context = {
+        "num_of_tasks": num_of_tasks,
+        "num_of_workers": num_of_workers,
+    }
+
+    return render(request, "manager/index.html", context=context)
