f0b6a304cfd51d180aa100992f1eb38f8c817669
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-09-25 20:40:34 +0200

    update views.py-update TaskListView and WorkerListView add search forms

--- manager/views.py
+++ manager/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from manager.forms import WorkerCreationForm, WorkerPositionUpdateForm
+from manager.forms import WorkerCreationForm, WorkerPositionUpdateForm, TaskNameSearchForm, WorkerUsernameSearchForm
 from manager.models import Task, Position, TaskType
 
 User = get_user_model()
@@ -118,7 +118,19 @@ class TaskTypeDeleteView(generic.DeleteView):
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
-    queryset = Task.objects.all().select_related("task_type")
+
+    def get_queryset(self):
+        form = TaskNameSearchForm(self.request.GET)
+        queryset = Task.objects.all().select_related("task_type")
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        context["search_form"] = TaskNameSearchForm(initial={"name": name})
+        return context
 
 
 class TaskDetailView(generic.DetailView):
@@ -166,6 +178,19 @@ class TaskDeleteView(generic.DeleteView):
 class WorkerListView(generic.ListView):
     model = User
     paginate_by = 5
+
+    def get_queryset(self):
+        form = WorkerUsernameSearchForm(self.request.GET)
+        queryset = User.objects.all()
+        if form.is_valid():
+            return queryset.filter(username__icontains=form.cleaned_data["username"])
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username")
+        context["search_form"] = WorkerUsernameSearchForm(initial={"username": username})
+        return context
 
 
 class WorkerDetailView(generic.DetailView):
